5f44ae9d627fd19f55df268dbf8b087d3e34c004
adamsinger82 <ADAMSINGER82@gmail.com>
2026-06-16 05:41:13 -0400

    replacemiddleware - loading page.js instead of the pdf for cme

--- src/middleware.js
+++ src/middleware.js
@@ -46,5 +46,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-matcher: ["/((?!_next/static|_next/image|favicon.ico|atlas/|images/).*)"],
+matcher: ["/((?!_next/static|_next/image|favicon.ico|atlas/|images/|pdf/).*)"],
 };
